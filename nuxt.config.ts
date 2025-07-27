41e7809ae9fb7e6cd3f902356d7d086a6d513e2c
Micko Kok <kokmicko@gmail.com>
2025-07-27 21:49:01 +0700

    new commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  app: {
+    baseURL: '/front/',
+  },
   vite: {
+    base: '/front/',
     plugins: [
       tailwindcss(),
     ],
